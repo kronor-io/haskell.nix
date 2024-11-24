f6255314582d29066e10fd660d618216f922dfa7
pranaysashank <pranaysashank@gmail.com>
2024-11-24 21:05:45 +0530

    remove stuff

--- overlays/emscripten.nix
+++
@@ -1,7 +0,0 @@
-final: _prev: {
-    emsdk = final.linkFarm "emsdk" [
-      { name = "upstream/bin"; path = final.clang + "/bin"; }
-      { name = "upstream/emscripten"; path = final.emscripten + "/bin"; }
-      { name = "share"; path = final.emscripten + "/share"; }
-    ];
-}
