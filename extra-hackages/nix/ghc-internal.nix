139e6c595b5f14de48ba2589fb51051b10efc70e
pranaysashank <pranaysashank@gmail.com>
2025-08-31 23:12:14 +0530

    Bump cabal to 3.16.0.0 and build it with ghc 9.12.2

---
+++ extra-hackages/nix/ghc-internal.nix
@@ -0,0 +1,14 @@
+{
+  "9.1202.0" = {
+    sha256 = "e799ce891e53432fccc9235a433372454dd910fbf3698ff7e8dce2c201f9e616";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/ghc-internal-9.1202.0-r0-60e5794fc814deae622641b9982cd9c9db929b6ea4f778fc70fbc10b572ad8df.nix;
+        revNum = 0;
+        revTimestamp = "2025-08-31T08:25:08Z";
+        sha256 = "60e5794fc814deae622641b9982cd9c9db929b6ea4f778fc70fbc10b572ad8df";
+      };
+      default = "r0";
+    };
+  };
+}
